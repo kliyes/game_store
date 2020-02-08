a9f808f231b943bf51f3d9fa3c8fb83d2bf3e7bf
Jing Yang <yang.jing@ubisoft.com>
2020-02-08 19:02:23 +0800

    fix deploy bash file

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env sh
 
-USER=root
-HOST=45.77.197.6
-BRANCH=master
-PROJECT=game_store
-PKG=game_store.tar.gz
-DEST=/opt
+export USER=root
+export HOST=45.77.197.6
+export BRANCH=master
+export PROJECT=game_store
+export PKG=game_store.tar.gz
+export DEST=/opt
 
 git archive -o $PKG $BRANCH
 scp -o StrictHostKeyChecking=no $PKG $USER@$HOST:$DEST
 ssh $USER@$HOST -o StrictHostKeyChecking=no '\
-  tar xf $DEST/$PKG && \
+  tar xf $DEST/$PKG -C $DEST && \
   cd $DEST/$PROJECT && \
   docker-compose -f docker-compose.yml up -d --build web'
