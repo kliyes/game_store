46ce510243072540312a308d508a76ef07fc03d9
Jing Yang <yang.jing@ubisoft.com>
2020-02-08 17:34:20 +0800

    move after_success scripts into a separate bash file

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+USER=root
+HOST=45.77.197.6
+BRANCH=master
+PROJECT=game_store
+PKG=game_store.tar.gz
+DEST=/opt
+
+git archive -o $PKG $BRANCH
+scp $PKG $USER@$HOST:$DEST
+ssh $USER@$HOST -o StrictHostKeyChecking=no <<EOF
+  tar xf $DEST/$PKG
+  cd $DEST/$PROJECT
+  docker-compose -f docker-compose.yml up -d --build web
+EOF
