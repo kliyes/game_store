93fedac23310b418ebbe4ea4ca494447b2437f7d
Jing Yang <yang.jing@ubisoft.com>
2020-02-08 20:01:16 +0800

    add docker compose production configuration file

--- deploy.sh
+++ deploy.sh
@@ -10,4 +10,4 @@ ssh $USER@$HOST -o StrictHostKeyChecking=no '\
   mkdir -p /opt/game_store
   tar xf /opt/game_store.tar.gz -C /opt/game_store && \
   cd /opt/game_store && \
-  docker-compose -f docker-compose.yml up -d --build web'
+  docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d --build web'
